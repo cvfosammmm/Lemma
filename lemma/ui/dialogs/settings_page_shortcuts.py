0b4df331c9670ad1458e858e1f3ab3dbaadab806
Robert Griesel <r.griesel@gmail.com>
2026-04-23 13:30:41 +0200

    choose custom shortcuts

--- lemma/ui/dialogs/settings_page_shortcuts.py
+++ lemma/ui/dialogs/settings_page_shortcuts.py
@@ -18,11 +18,12 @@
 import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw
+from gi.repository import Gtk, Adw, Gdk
 
 from lemma.services.settings import Settings
 from lemma.use_cases.use_cases import UseCases
 from lemma.ui.shortcuts import Shortcuts
+from lemma.ui.dialogs.dialog_view_action import DialogViewAction
 
 
 class PageShortcuts(object):
@@ -31,88 +32,169 @@ class PageShortcuts(object):
         self.settings = settings
         self.main_window = main_window
 
-        self.data = list()
+        self.buttons = dict()
 
-        section = {'title': 'Windows and Panels', 'items': list()}
-        section['items'].append(['Show Global Menu', Shortcuts.get_trigger_string('show_hamburger_menu')])
-        section['items'].append(['Show Document Menu', Shortcuts.get_trigger_string('show_document_menu')])
-        section['items'].append(['Show Keyboard Shortcuts', Shortcuts.get_trigger_string('show_shortcuts_dialog')])
-        section['items'].append(['Quit the Application', Shortcuts.get_trigger_string('quit')])
-        self.data.append(section)
+        self.sections = list()
+        self.sections.append({'title': 'Windows and Panels', 'items': ['show_hamburger_menu', 'show_document_menu', 'show_shortcuts_dialog', 'quit']})
+        self.sections.append({'title': 'Documents', 'items': ['add_document', 'rename_document']})
+        self.sections.append({'title': 'Navigation', 'items': ['start_global_search', 'go_back', 'go_forward', 'show_bookmarks', 'activate_bookmark_1', 'activate_bookmark_2', 'activate_bookmark_3', 'activate_bookmark_4', 'activate_bookmark_5', 'activate_bookmark_6', 'activate_bookmark_7', 'activate_bookmark_8', 'activate_bookmark_9']})
+        self.sections.append({'title': 'Undo and Redo', 'items': ['undo', 'redo']})
+        self.sections.append({'title': 'Copy and Paste', 'items': ['cut', 'copy', 'paste']})
+        self.sections.append({'title': 'Actions', 'items': ['toggle_checkbox']})
+        self.sections.append({'title': 'Formatting', 'items': ['toggle_bold', 'toggle_italic', 'toggle_verbatim', 'toggle_highlight', 'paragraph_style_h2', 'paragraph_style_h3', 'paragraph_style_h4', 'paragraph_style_h5', 'paragraph_style_h6', 'paragraph_style_ul', 'paragraph_style_ol', 'paragraph_style_cl', 'paragraph_style_p']})
+        self.sections.append({'title': 'Math', 'items': ['subscript', 'superscript']})
 
-        section = {'title': 'Documents', 'items': list()}
-        section['items'].append(['Create new Document', Shortcuts.get_trigger_string('add_document')])
-        section['items'].append(['Rename Document', Shortcuts.get_trigger_string('rename_document')])
-        self.data.append(section)
-
-        section = {'title': 'Navigation', 'items': list()}
-        section['items'].append(['Search', Shortcuts.get_trigger_string('start_global_search')])
-        section['items'].append(['Go Back', Shortcuts.get_trigger_string('go_back')])
-        section['items'].append(['Go Forward', Shortcuts.get_trigger_string('go_forward')])
-        section['items'].append(['Show Bookmarks', Shortcuts.get_trigger_string('show_bookmarks')])
-        section['items'].append(['Open Bookmark 1', Shortcuts.get_trigger_string('activate_bookmark_1')])
-        section['items'].append(['Open Bookmark 2', Shortcuts.get_trigger_string('activate_bookmark_2')])
-        section['items'].append(['Open Bookmark 3', Shortcuts.get_trigger_string('activate_bookmark_3')])
-        section['items'].append(['Open Bookmark 4', Shortcuts.get_trigger_string('activate_bookmark_4')])
-        section['items'].append(['Open Bookmark 5', Shortcuts.get_trigger_string('activate_bookmark_5')])
-        section['items'].append(['Open Bookmark 6', Shortcuts.get_trigger_string('activate_bookmark_6')])
-        section['items'].append(['Open Bookmark 7', Shortcuts.get_trigger_string('activate_bookmark_7')])
-        section['items'].append(['Open Bookmark 8', Shortcuts.get_trigger_string('activate_bookmark_8')])
-        section['items'].append(['Open Bookmark 9', Shortcuts.get_trigger_string('activate_bookmark_9')])
-        self.data.append(section)
-
-        section = {'title': 'Undo and Redo', 'items': list()}
-        section['items'].append(['Undo', Shortcuts.get_trigger_string('undo')])
-        section['items'].append(['Redo', Shortcuts.get_trigger_string('redo')])
-        self.data.append(section)
-
-        section = {'title': 'Copy and Paste', 'items': list()}
-        section['items'].append(['Cut', Shortcuts.get_trigger_string('cut')])
-        section['items'].append(['Copy', Shortcuts.get_trigger_string('copy')])
-        section['items'].append(['Paste', Shortcuts.get_trigger_string('paste')])
-        self.data.append(section)
-
-        section = {'title': 'Actions', 'items': list()}
-        section['items'].append(['Toggle Checkbox', Shortcuts.get_trigger_string('toggle_checkbox')])
-        self.data.append(section)
-
-        section = {'title': 'Formatting', 'items': list()}
-        section['items'].append(['Bold Text', Shortcuts.get_trigger_string('toggle_bold')])
-        section['items'].append(['Italic Text', Shortcuts.get_trigger_string('toggle_italic')])
-        section['items'].append(['Verbatim Text', Shortcuts.get_trigger_string('toggle_verbatim')])
-        section['items'].append(['Highlight Text', Shortcuts.get_trigger_string('toggle_highlight')])
-        section['items'].append(['Heading 2', Shortcuts.get_trigger_string('paragraph_style_h2')])
-        section['items'].append(['Heading 3', Shortcuts.get_trigger_string('paragraph_style_h3')])
-        section['items'].append(['Heading 4', Shortcuts.get_trigger_string('paragraph_style_h4')])
-        section['items'].append(['Heading 5', Shortcuts.get_trigger_string('paragraph_style_h5')])
-        section['items'].append(['Heading 6', Shortcuts.get_trigger_string('paragraph_style_h6')])
-        section['items'].append(['Bullet List', Shortcuts.get_trigger_string('paragraph_style_ul')])
-        section['items'].append(['Numbered List', Shortcuts.get_trigger_string('paragraph_style_ol')])
-        section['items'].append(['Checklist', Shortcuts.get_trigger_string('paragraph_style_cl')])
-        section['items'].append(['Normal Paragraph', Shortcuts.get_trigger_string('paragraph_style_p')])
-        self.data.append(section)
-
-        section = {'title': 'Math', 'items': list()}
-        section['items'].append(['Subscript', Shortcuts.get_trigger_string('subscript')])
-        section['items'].append(['Superscript', Shortcuts.get_trigger_string('superscript')])
-        self.data.append(section)
-
-        self.view = PageShortcutsView(self.data)
+        self.view = PageShortcutsView()
 
     def init(self):
-        return
-        for i, section in enumerate(self.button_list):
-            for item in section:
-                self.view.checkboxes[item[1]].connect('toggled', self.on_checkbutton_toggled, item[1])
-                self.view.checkboxes[item[1]].set_active(Settings.get_value(item[1]))
+        is_first = True
+        for section in self.sections:
+            subheader = Gtk.Label.new(section['title'])
+            subheader.add_css_class('settings-header')
+            subheader.set_xalign(0)
+            if is_first:
+                subheader.add_css_class('first')
+                is_first = False
 
-    def on_checkbutton_toggled(self, button, key):
-        UseCases.settings_set_value(key, button.get_active())
+            self.view.vbox.append(subheader)
+
+            for item in section['items']:
+                box = Gtk.CenterBox()
+                box.set_start_widget(Gtk.Label.new(_(Shortcuts.get_title(item))))
+                box.set_end_widget(Adw.ShortcutLabel.new(Shortcuts.get_trigger_string(item)))
+                button = Gtk.Button()
+                button.add_css_class('item')
+                button.add_css_class('flat')
+                button.set_child(box)
+                button.connect('clicked', self.on_item_clicked, item)
+                self.buttons[item] = button
+
+                self.view.vbox.append(button)
+
+        self.view.reset_button.connect('clicked', self.on_reset_clicked)
+
+    def on_item_clicked(self, button, item):
+        self.dialog = SetShortcutDialog(self.settings.view)
+        self.dialog.cancel_button.connect('clicked', self.on_dialog_cancel_clicked)
+        self.dialog.submit_button.connect('clicked', self.on_dialog_submit_clicked)
+        self.dialog.shortcuts_controller.add_cb('submit_dialog', self.submit)
+        self.dialog.run(item)
+
+    def on_dialog_cancel_clicked(self, button):
+        self.dialog.close()
+
+    def on_dialog_submit_clicked(self, button):
+        self.submit()
+
+    def submit(self):
+        trigger = self.dialog.trigger_string
+        if trigger != '':
+            sc_with_trigger = Shortcuts.get_name_by_trigger_string(trigger)
+            if sc_with_trigger != None:
+                Shortcuts.set_shortcut(sc_with_trigger, '')
+
+        Shortcuts.set_shortcut(self.dialog.key, self.dialog.trigger_string)
+        self.dialog.close()
+
+        box = self.buttons[self.dialog.key].get_child()
+        box.set_end_widget(Adw.ShortcutLabel.new(Shortcuts.get_trigger_string(self.dialog.key)))
+
+    def on_reset_clicked(self, button):
+        Shortcuts.reset_to_defaults()
+
+        for key, button in self.buttons.items():
+            button.get_child().set_end_widget(Adw.ShortcutLabel.new(Shortcuts.get_trigger_string(key)))
+
+
+class SetShortcutDialog(DialogViewAction):
+
+    def __init__(self, main_dialog):
+        DialogViewAction.__init__(self, main_dialog, _('Set Shortcut'), 450, 'set-shortcut-dialog', _('Set Shortcut'))
+        self.add_css_class('no-border')
+        self.add_css_class('set-shortcut-dialog')
+
+        self.explainer_label = Gtk.Label()
+        self.explainer_label.set_xalign(0)
+        self.explainer_label.set_margin_top(6)
+        self.explainer_label.add_css_class('settings-margin-box')
+        self.content.append(self.explainer_label)
+
+        self.shortcuts_label_box_inner = Gtk.CenterBox()
+        self.shortcuts_label_box_outer = Gtk.CenterBox()
+        self.shortcuts_label_box_outer.set_orientation(Gtk.Orientation.VERTICAL)
+        self.shortcuts_label_box_outer.set_size_request(-1, 290)
+        self.shortcuts_label_box_outer.set_center_widget(self.shortcuts_label_box_inner)
+        self.content.append(self.shortcuts_label_box_outer)
+
+        self.warning_label = Gtk.Label()
+        self.warning_label.set_wrap(True)
+        self.warning_label.set_xalign(0)
+        self.warning_label.add_css_class('warning-box')
+
+        self.warning_label_revealer = Gtk.Revealer()
+        self.warning_label_revealer.set_child(self.warning_label)
+        self.warning_label_revealer.set_transition_type(Gtk.RevealerTransitionType.NONE)
+        self.warning_label_revealer.set_reveal_child(False)
+
+        self.content.append(self.warning_label_revealer)
+
+        self.reset_button = Gtk.Button.new_with_label('Remove')
+        self.reset_button.add_css_class('destructive-action')
+        self.reset_button.set_sensitive(False)
+        self.reset_button.connect('clicked', self.on_reset_button_clicked)
+        self.action_bar.pack_start(self.reset_button)
+
+        self.key = None
+        self.trigger_string = ''
+        self.key_controller = Gtk.EventControllerKey()
+        self.key_controller.connect('key-pressed', self.on_keypress)
+        self.add_controller(self.key_controller)
+
+    def run(self, key):
+        self.key = key
+        self.explainer_label.set_markup('Enter a new shortcut for <b>' + Shortcuts.get_title(key) + '</b>.')
+        self.trigger_string = Shortcuts.get_trigger_string(key)
+        self.update()
+        self.present()
+
+    def on_keypress(self, controller, keyval, keycode, keyboard_state):
+        if Gdk.keyval_name(keyval).startswith('Control'): return
+        if Gdk.keyval_name(keyval).startswith('Shift'): return
+        if Gdk.keyval_name(keyval).startswith('Alt'): return
+        if Gdk.keyval_name(keyval).startswith('Super'): return
+        if Gdk.keyval_name(keyval).startswith('Return'): return
+
+        self.trigger_string = Gtk.accelerator_name(keyval, keyboard_state)
+        self.update()
+
+        return True
+
+    def on_reset_button_clicked(self, button):
+        self.trigger_string = ''
+        self.update()
+
+    def update(self):
+        if self.trigger_string != '':
+            sc_with_trigger = Shortcuts.get_name_by_trigger_string(self.trigger_string)
+            if sc_with_trigger != None and sc_with_trigger != self.key:
+                self.warning_label.set_markup('<b>' + Shortcuts.get_for_labels(sc_with_trigger) + _('</b> is already being used for <i>') + Shortcuts.get_title(sc_with_trigger) + _('</i>. If you set it, <i>') + Shortcuts.get_title(sc_with_trigger) + _('</i> will be disabled.'))
+                self.warning_label_revealer.set_reveal_child(True)
+            else:
+                self.warning_label_revealer.set_reveal_child(False)
+
+            self.shortcuts_label_box_inner.set_center_widget(Adw.ShortcutLabel.new(self.trigger_string))
+            self.reset_button.set_sensitive(True)
+            self.submit_button.set_label(_('Set Shortcut'))
+        else:
+            self.warning_label_revealer.set_reveal_child(False)
+            self.shortcuts_label_box_inner.set_center_widget(None)
+            self.reset_button.set_sensitive(False)
+            self.submit_button.set_label(_('Set No Shortcut'))
 
 
 class PageShortcutsView(Gtk.Box):
 
-    def __init__(self, data):
+    def __init__(self):
         Gtk.Box.__init__(self)
         self.set_orientation(Gtk.Orientation.VERTICAL)
         self.get_style_context().add_class('settings-page')
@@ -120,25 +202,25 @@ class PageShortcutsView(Gtk.Box):
 
         self.vbox = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0)
 
-        for section in data:
-            subheader = Gtk.Label.new(section['title'])
-            subheader.add_css_class('settings-header')
-            subheader.set_xalign(0)
-            self.vbox.append(subheader)
-
-            for item in section['items']:
-                box = Gtk.CenterBox()
-                box.set_start_widget(Gtk.Label.new(_(item[0])))
-                box.set_end_widget(Adw.ShortcutLabel.new(_(item[1])))
-                box.add_css_class('item')
-
-                self.vbox.append(box)
-
         self.scrolled_window = Gtk.ScrolledWindow()
         self.scrolled_window.set_propagate_natural_height(True)
         self.scrolled_window.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         self.scrolled_window.set_child(self.vbox)
 
+        self.reset_button = Gtk.Button.new_with_label('Reset to Defaults')
+        self.reset_button.add_css_class('destructive-action')
+        self.reset_button.add_css_class('flat')
+
+        info_label = Gtk.Label.new(_('Click on the shortcuts above to change them. You may have to restart Lemma for changes to take full effect.'))
+        info_label.set_xalign(0)
+        info_label.set_wrap(True)
+
+        bottom_box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0)
+        bottom_box.add_css_class('settings-bottom-box')
+        bottom_box.append(info_label)
+        bottom_box.append(self.reset_button)
+
         self.append(self.scrolled_window)
+        self.append(bottom_box)
 
 
--- lemma/ui/shortcuts.py
+++ lemma/ui/shortcuts.py
@@ -17,19 +17,23 @@
 
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
+
+import os.path, pickle
 
 from lemma.services.files import Files
 
 
 class Shortcuts():
 
+    controllers = list()
     defaults = dict()
     data = dict()
+    titles = dict()
 
     def init():
         Shortcuts.defaults['close_dialog'] = 'Escape'
-
+        Shortcuts.defaults['submit_dialog'] = 'Return'
         Shortcuts.defaults['toggle_bold'] = '<Control>b'
         Shortcuts.defaults['toggle_italic'] = '<Control>i'
         Shortcuts.defaults['toggle_verbatim'] = '<Control>e'
@@ -54,7 +58,6 @@ class Shortcuts():
         Shortcuts.defaults['paragraph_style_cl'] = '<Control>9'
         Shortcuts.defaults['paragraph_style_p'] = '<Control>0'
         Shortcuts.defaults['rename_document'] = 'F2'
-
         Shortcuts.defaults['quit'] = '<Control>q'
         Shortcuts.defaults['add_document'] = '<Control>n'
         Shortcuts.defaults['go_back'] = '<Alt>Left'
@@ -65,14 +68,75 @@ class Shortcuts():
         Shortcuts.defaults['show_bookmarks'] = '<Alt>0'
         for i in range(1, 10):
             Shortcuts.defaults['activate_bookmark_' + str(i)] = '<Alt>' + str(i)
-
         Shortcuts.defaults['start_global_search'] = '<Control>f'
         Shortcuts.defaults['stop_global_search'] = 'Escape'
         Shortcuts.defaults['global_search_prev_result'] = 'Up'
         Shortcuts.defaults['global_search_next_result'] = 'Down'
 
+        Shortcuts.titles['show_hamburger_menu'] = 'Show Global Menu'
+        Shortcuts.titles['show_document_menu'] = 'Show Document Menu'
+        Shortcuts.titles['show_shortcuts_dialog'] = 'Show Keyboard Shortcuts'
+        Shortcuts.titles['quit'] = 'Quit the Application'
+        Shortcuts.titles['add_document'] = 'Create new Document'
+        Shortcuts.titles['rename_document'] = 'Rename Document'
+        Shortcuts.titles['start_global_search'] = 'Search'
+        Shortcuts.titles['go_back'] = 'Go Back'
+        Shortcuts.titles['go_forward'] = 'Go Forward'
+        Shortcuts.titles['show_bookmarks'] = 'Show Bookmarks'
+        Shortcuts.titles['activate_bookmark_1'] = 'Open Bookmark 1'
+        Shortcuts.titles['activate_bookmark_2'] = 'Open Bookmark 2'
+        Shortcuts.titles['activate_bookmark_3'] = 'Open Bookmark 3'
+        Shortcuts.titles['activate_bookmark_4'] = 'Open Bookmark 4'
+        Shortcuts.titles['activate_bookmark_5'] = 'Open Bookmark 5'
+        Shortcuts.titles['activate_bookmark_6'] = 'Open Bookmark 6'
+        Shortcuts.titles['activate_bookmark_7'] = 'Open Bookmark 7'
+        Shortcuts.titles['activate_bookmark_8'] = 'Open Bookmark 8'
+        Shortcuts.titles['activate_bookmark_9'] = 'Open Bookmark 9'
+        Shortcuts.titles['undo'] = 'Undo'
+        Shortcuts.titles['redo'] = 'Redo'
+        Shortcuts.titles['cut'] = 'Cut'
+        Shortcuts.titles['copy'] = 'Copy'
+        Shortcuts.titles['paste'] = 'Paste'
+        Shortcuts.titles['toggle_checkbox'] = 'Toggle Checkbox'
+        Shortcuts.titles['toggle_bold'] = 'Bold Text'
+        Shortcuts.titles['toggle_italic'] = 'Italic Text'
+        Shortcuts.titles['toggle_verbatim'] = 'Verbatim Text'
+        Shortcuts.titles['toggle_highlight'] = 'Highlight Text'
+        Shortcuts.titles['paragraph_style_h2'] = 'Heading 2'
+        Shortcuts.titles['paragraph_style_h3'] = 'Heading 3'
+        Shortcuts.titles['paragraph_style_h4'] = 'Heading 4'
+        Shortcuts.titles['paragraph_style_h5'] = 'Heading 5'
+        Shortcuts.titles['paragraph_style_h6'] = 'Heading 6'
+        Shortcuts.titles['paragraph_style_ul'] = 'Bullet List'
+        Shortcuts.titles['paragraph_style_ol'] = 'Numbered List'
+        Shortcuts.titles['paragraph_style_cl'] = 'Checklist'
+        Shortcuts.titles['paragraph_style_p'] = 'Normal Paragraph'
+        Shortcuts.titles['subscript'] = 'Subscript'
+        Shortcuts.titles['superscript'] = 'Superscript'
+
+        Shortcuts.load_from_disk()
+
+    def set_shortcut(name, trigger_string):
+        Shortcuts.data[name] = trigger_string
+
+        for controller in Shortcuts.controllers:
+            controller.update_trigger(name)
+
+        Shortcuts.save_to_disk()
+
+    def reset_to_defaults():
+        Shortcuts.data = dict()
+
+        for controller in Shortcuts.controllers:
+            controller.update_triggers()
+
+        Shortcuts.save_to_disk()
+
     def new_controller():
-        return ShortcutController()
+        controller = ShortcutController()
+
+        Shortcuts.controllers.append(controller)
+        return controller
 
     def get_trigger_string(name):
         try: value = Shortcuts.data[name]
@@ -81,51 +145,44 @@ class Shortcuts():
 
         return value
 
+    def get_name_by_trigger_string(trigger_string):
+        for key in Shortcuts.defaults:
+            if Shortcuts.get_trigger_string(key) == trigger_string:
+                return key
+        return None
+
+    def get_title(name):
+        try: value = Shortcuts.titles[name]
+        except KeyError:
+            value = name
+        return value
+
     def get_for_labels(name):
         try: value = Shortcuts.data[name]
         except KeyError:
             value = Shortcuts.defaults[name]
+        return Gtk.ShortcutTrigger.parse_string(value).to_label(Gdk.Display.get_default())
 
-        value = value.replace('minus', '-')
-        value = value.replace('underscore', '_')
-        value = value.replace('question', '?')
+    def load_from_disk():
+        try: filehandle = open(os.path.join(Files.get_config_folder(), 'shortcuts.pickle'), 'rb')
+        except IOError: return False
+        else:
+            try: Shortcuts.data = pickle.load(filehandle)
+            except EOFError: return False
+        return True
 
-        value = value.replace('>a', '>A')
-        value = value.replace('>b', '>B')
-        value = value.replace('>c', '>C')
-        value = value.replace('>d', '>D')
-        value = value.replace('>e', '>E')
-        value = value.replace('>f', '>F')
-        value = value.replace('>g', '>G')
-        value = value.replace('>h', '>H')
-        value = value.replace('>i', '>I')
-        value = value.replace('>j', '>J')
-        value = value.replace('>k', '>K')
-        value = value.replace('>l', '>L')
-        value = value.replace('>m', '>M')
-        value = value.replace('>n', '>N')
-        value = value.replace('>o', '>O')
-        value = value.replace('>p', '>P')
-        value = value.replace('>q', '>Q')
-        value = value.replace('>r', '>R')
-        value = value.replace('>s', '>S')
-        value = value.replace('>t', '>T')
-        value = value.replace('>u', '>U')
-        value = value.replace('>v', '>V')
-        value = value.replace('>w', '>W')
-        value = value.replace('>x', '>X')
-        value = value.replace('>y', '>Y')
-        value = value.replace('>z', '>Z')
-
-        value = value.replace('Escape', 'Esc')
-
-        return value.replace('<Control>', 'Ctrl+').replace('<Shift>', 'Shift+').replace('<Alt>', 'Alt+')
+    def save_to_disk():
+        try: filehandle = open(os.path.join(Files.get_config_folder(), 'shortcuts.pickle'), 'wb')
+        except IOError: return False
+        else: pickle.dump(Shortcuts.data, filehandle)
 
 
 class ShortcutController(Gtk.ShortcutController):
 
     def __init__(self):
         Gtk.ShortcutController.__init__(self)
+
+        self.shortcuts_by_name = dict()
 
         self.set_propagation_phase(Gtk.PropagationPhase.CAPTURE)
 
@@ -137,7 +194,19 @@ class ShortcutController(Gtk.ShortcutController):
         shortcut.set_action(callback_action)
         shortcut.set_trigger(Gtk.ShortcutTrigger.parse_string(trigger_string))
 
+        self.shortcuts_by_name[name] = shortcut
+
         self.add_shortcut(shortcut)
+
+    def update_trigger(self, name):
+        if name in self.shortcuts_by_name:
+            trigger_string = Shortcuts.get_trigger_string(name)
+            self.shortcuts_by_name[name].set_trigger(Gtk.ShortcutTrigger.parse_string(trigger_string))
+
+    def update_triggers(self):
+        for name in self.shortcuts_by_name:
+            trigger_string = Shortcuts.get_trigger_string(name)
+            self.shortcuts_by_name[name].set_trigger(Gtk.ShortcutTrigger.parse_string(trigger_string))
 
     def action(self, a, b, arguments):
         callback, data = arguments
